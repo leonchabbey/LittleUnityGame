0e4cc89fca22ec0d964416d8cfda90453c475741
Leon Chabbey <leon.chabbey@gmail.com>
2017-11-02 19:06:24 +0100

    added ennemy

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    [Header("Bullet")]
+    [SerializeField]
+    private GameObject bulletPrefab;
+    [SerializeField]
+    private float bulletVelocity = 10;
+
+    [Header("Guns")]
+    [SerializeField]
+    private Transform[] gunTransformList;
+    [SerializeField]
+    private float timeToFire = 2;
+    private float lastTimeFire;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Fire());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private IEnumerator Fire() {
+        while(true) {
+            yield return new WaitForSeconds(timeToFire);
+
+            foreach(Transform gun in gunTransformList) {
+                GameObject bullet = Instantiate(bulletPrefab, gun.position, gun.rotation);
+                bullet.GetComponent<Rigidbody2D>().velocity = gun.right * bulletVelocity;
+                Destroy(bullet, 5);
+            }
+        }
+    }
+}
