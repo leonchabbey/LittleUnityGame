ba62a8c0c9c3052a0ce55d1f73d380e5f1aa3b36
Leon Chabbey <leon.chabbey@gmail.com>
2017-11-02 19:23:59 +0100

    added ui

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    private int life = 3;
+
+    [SerializeField]
+    private Text textLife;
+
+    private const string TEXT_LIFE = "Life: ";
+
+	// Use this for initialization
+	void Start () {
+        updateTextLife();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayerDie() {
+        if(life <= 0) {
+            SceneManager.LoadScene("StartMenu");
+        } else {
+            life--;
+            updateTextLife();
+        }
+    }
+
+    void updateTextLife()
+    {
+        textLife.text = TEXT_LIFE + life;
+    }
+}
