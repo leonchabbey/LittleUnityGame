739f3545f8186f38f90dd8727c4b1f139a4502f8
Leon Chabbey <leon.chabbey@gmail.com>
2017-11-03 17:41:37 +0100

    amazing game arts added

---
+++ Assets/Scripts/StartGameButtonController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartGameButtonController : MonoBehaviour {
+
+    [SerializeField]
+    private Button button;
+
+	// Use this for initialization
+	void Start () {
+        Button startGameButton = button.GetComponent<Button>();
+        startGameButton.onClick.AddListener(startGame);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void startGame() {
+        SceneManager.LoadScene("FirstLevel");
+    }
+}
